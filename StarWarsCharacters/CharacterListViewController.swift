def368921be5d1c0c0d4025a46ccbeca0f0e7652
Ryan Plitt <ryanplitt@gmail.com>
2017-09-12 07:09:47 -0600

    Added swift files and updated storyboard

---
+++ StarWarsCharacters/CharacterListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CharacterListViewController.swift
+//  StarWarsCharacters
+//
+//  Created by Ryan Plitt on 9/12/17.
+//  Copyright © 2017 Ryan Plitt. All rights reserved.
+//
+
+import UIKit
+
+class CharacterListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StarWarsCharacters/CharacterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CharacterTableViewCell.swift
+//  StarWarsCharacters
+//
+//  Created by Ryan Plitt on 9/12/17.
+//  Copyright © 2017 Ryan Plitt. All rights reserved.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
